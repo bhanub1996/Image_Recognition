44bcccacb0781cf5bd40270fec1780af4315d236
Bhanu Prakash <80862844+bhanub1996@users.noreply.github.com>
2023-03-22 07:33:44 +0530

    Image Tagging Added

--- lib/mapbox_fun.py
+++ lib/mapbox_fun.py
@@ -10,3 +10,13 @@ def mapbox_reverse(lat, lon):
     response = mapbox.Geocoder(access_token=token).reverse(lon=lon, lat=lat)    
     # print(response.json()['features'][0]['place_name'])    
     return response.json()
+
+def mapbox_forward(search_text):
+    
+    token = "pk.eyJ1IjoiZ2hvdXNlcyIsImEiOiJjbGZnbjBscjAwcHBpM3RvOTdxYndqdTB4In0.-xhYjOzdVy7eAIl3wNrFog"    
+    responses.add(responses.GET,'https://api.mapbox.com/geocoding/v5/mapbox.places/{search_text}.json?access_token={token}',match_querystring=True,status=200,content_type='application/json')    
+    response = mapbox.Geocoder(access_token=token).forward(address=search_text)   
+    # print(response.json()['features'][0]['place_name'])    
+    return response.json()
+
+# https://api.mapbox.com/geocoding/v5/{endpoint}/{search_text}.json
--- main.py
+++ main.py
@@ -1,39 +1,41 @@
-import os
-from lib.exif import Exif
-from lib.mapbox_fun import mapbox_reverse
+# import os
+# from lib.exif import Exif
+# from lib.mapbox_fun import mapbox_reverse, mapbox_forward
+from lib.gcp_vision import vision_annotate 
 
 
-def main():
-    cwd = os.getcwd()
+# def main():
+#     cwd = os.getcwd()
     
-    os.chdir(os.path.join(cwd, "Images"))
-    files = os.listdir()
+#     os.chdir(os.path.join(cwd, "Images"))
+#     files = os.listdir()
       
     
-    if len(files) == 0:    
-        print("You don't have have files in the ./images folder.")    
-        exit()
+#     if len(files) == 0:    
+#         print("You don't have have files in the ./images folder.")    
+#         exit()
         
-    # Loop through the files in the Images directory:
+#     # Loop through the files in the Images directory:
 
-    for file in files:
-        file = os.path.join(os.getcwd(),file)
-        try:
-            # image_path = str(input("ENTER_IMAGE_PATH:"))
-            exif = Exif()
+#     for file in files:
+#         file = os.path.join(os.getcwd(),file)
+#         try:
+#             # image_path = str(input("ENTER_IMAGE_PATH:"))
+#             exif = Exif()
             
-            exif_data = exif.extract_data(file)
-            # print(exif_data)
-            if exif_data is not None:
-                if 'gps_coords' in exif_data:      
-                    result = mapbox_reverse(exif_data['gps_coords']['lat'], exif_data['gps_coords']['lon'])   
-                    #POI from Lat Lon             
-                    print(f"{file}:{exif_data['gps_coords']}::POI={result['features'][0]['place_name']}")  
-                        # 
+#             exif_data = exif.extract_data(file)
+#             # print(exif_data)
+#             if exif_data is not None:
+#                 if 'gps_coords' in exif_data:      
+#                     result = mapbox_reverse(exif_data['gps_coords']['lat'], exif_data['gps_coords']['lon'])   
+#                     #POI from Lat Lon             
+#                     print(f"{file}:{exif_data['gps_coords']}::POI={result['features'][0]['place_name']}")
+#                     result = mapbox_forward(result['features'][0]['place_name'])  
+#                     print(result)
             
-        except IOError:        
-            print(f"File format not supported for the given file: {file}") 
+#         except IOError:        
+#             print(f"File format not supported for the given file: {file}") 
     
-                                                             
-if __name__ == "__main__":
-    main()
+                                                            
+print(vision_annotate())
+
